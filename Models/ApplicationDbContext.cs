048e57433db34382915b3aeef7e96a4e3ee53cd7
emirhancebiroglu <152030621+emirhancebiroglu@users.noreply.github.com>
2024-08-14 23:25:04 +0300

    .

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -4,7 +4,5 @@ namespace WebRTCWebSocketServer.Models
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
     {
-        public DbSet<CallRecording> CallRecordings { get; set; }
-        public DbSet<RecordingFile> RecordingFiles { get; set; }
     }
 }
